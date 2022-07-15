39ab2664aa0595b04efee9621b24cab930c50b5b
bruno <brunotheodoro123@gmail.com>
2022-07-15 09:08:41 -0300

    fix: add dotenv

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,8 +1,11 @@
 import 'reflect-metadata';
+import dotenv from 'dotenv';
+
 import { db } from '../sequelize/db';
 import { app } from './express';
 import '../../container';
 
+dotenv.config();
 const port = process.env.PORT || 3333;
 
 app.listen(port, async () => {
