d8ce020515680eee7af9141ff208a523a2413f36
bruno <brunotheodoro123@gmail.com>
2022-07-15 09:42:40 -0300

    fix: fix  heroku

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,11 +1,22 @@
 import 'reflect-metadata';
 import dotenv from 'dotenv';
+import { Sequelize } from 'sequelize';
 
-import { db } from '../sequelize/db';
 import { app } from './express';
 import '../../container';
 
 dotenv.config();
+const dbUrl = process.env.DATABASE_URL
+  ? process.env.DATABASE_URL
+  : 'postgres://docker:bruno@localhost:5432/database';
+
+const db = new Sequelize(dbUrl, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  dialectOptions: {
+    ssl: true,
+  },
+});
 
 app.listen(process.env.PORT || 3333, async () => {
   await db.sync();
