756e7d02b8b8adf31f4224e636c2af0efc7db846
bruno <brunotheodoro123@gmail.com>
2022-07-15 10:46:31 -0300

    fix: fix  heroku

--- src/modules/User/respositories/IUsersRepository.ts
+++ src/modules/User/respositories/IUsersRepository.ts
@@ -1,9 +1,7 @@
-import { Model } from 'sequelize/types';
-
 import { ICreateUserDTO } from '../dtos/ICreateUserDTO';
 
 export interface IUsersRepository {
   create(entity: ICreateUserDTO): Promise<void>;
-  findByUser(user: string): Promise<Model>;
-  findById(id: string): Promise<Model>;
+  findByUser(user: string);
+  findById(id: string);
 }
