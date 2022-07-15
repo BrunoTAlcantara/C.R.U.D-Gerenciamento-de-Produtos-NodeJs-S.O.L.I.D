384f4a6e7999a01054794562bed8de0f1a5745b4
bruno <brunotheodoro123@gmail.com>
2022-07-15 09:26:42 -0300

    fix: fix  heroku

--- src/shared/infra/sequelize/db.ts
+++ src/shared/infra/sequelize/db.ts
@@ -2,7 +2,9 @@ import dotenv from 'dotenv';
 import { Sequelize } from 'sequelize';
 
 dotenv.config();
-const dbUrl = process.env.DATABASE_URL;
+const dbUrl = process.env.DATABASE_URL
+  ? process.env.DATABASE_URL
+  : 'postgres://docker:bruno@localhost:5432/database';
 
 export const db = new Sequelize(dbUrl, {
   dialect: 'postgres',
