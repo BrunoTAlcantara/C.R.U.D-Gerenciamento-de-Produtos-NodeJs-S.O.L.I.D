eb643dea9429c8c72924f400dc8eff1cc0a28268
bruno <brunotheodoro123@gmail.com>
2022-07-16 16:02:12 -0300

    fix: controllerProduct

--- src/shared/infra/http/routes/products.routes.ts
+++ src/shared/infra/http/routes/products.routes.ts
@@ -1,16 +1,17 @@
 import { Router } from 'express';
 
-import { DeleteClientsController } from '../../../../modules/Clients/UseCases/DeleteClients/DeleteClientsController';
-import { ListClientsController } from '../../../../modules/Clients/UseCases/ListClients/ListClientsController';
-import { UpdateClientsController } from '../../../../modules/Clients/UseCases/UpdateClients/UpdateClientsController';
+
+
+
 import { CreateProductController } from '../../../../modules/Products/useCases/CreateProduct/CreateProductController';
 import { DeleteProductController } from '../../../../modules/Products/useCases/DeleteProduct/DeleteProductController';
+import { ListProductController } from '../../../../modules/Products/useCases/ListProduct/ListClientsController';
 import { UpdateProductController } from '../../../../modules/Products/useCases/UpdateClients/UpdateClientsController';
 
 const productRoutes = Router();
 
 const createProductController = new CreateProductController();
-const listProductController = new ListClientsController();
+const listProductController = new ListProductController();
 const updateProductController = new UpdateProductController();
 const deleteProductController = new DeleteProductController();
 
